101b16292a8533fd3bbee2611258b092fe1159c7
Samuel Ssekizinvu <73385715+samuelkchris@users.noreply.github.com>
2023-02-12 23:14:15 +0300

    test file

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,7 +5,6 @@ import unittest
 from datetime import datetime
 import time
 from models.user import User
-import re
 import json
 from models.engine.file_storage import FileStorage
 import os
@@ -14,40 +13,47 @@ from models.base_model import BaseModel
 
 
 class TestUser(unittest.TestCase):
-
-    """Test Cases for the User class."""
-
     def setUp(self):
         """Sets up test methods."""
-        pass
+        self.user = User()
 
     def tearDown(self):
         """Tears down test methods."""
-        self.resetStorage()
-        pass
-
-    def resetStorage(self):
-        """Resets FileStorage data."""
         FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
+        os.remove(FileStorage._FileStorage__file_path)
 
-    def test_8_instantiation(self):
+    def test_instantiation(self):
         """Tests instantiation of User class."""
+        self.assertIsInstance(self.user, User)
+        self.assertTrue(issubclass(type(self.user), BaseModel))
 
-        b = User()
-        self.assertEqual(str(type(b)), "<class 'models.user.User'>")
-        self.assertIsInstance(b, User)
-        self.assertTrue(issubclass(type(b), BaseModel))
-
-    def test_8_attributes(self):
+    def test_attributes(self):
         """Tests the attributes of User class."""
         attributes = storage.attributes()["User"]
-        o = User()
-        for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
+        for attr_name, attr_type in attributes.items():
+            self.assertTrue(hasattr(self.user, attr_name))
+            self.assertIsInstance(getattr(self.user, attr_name, None), attr_type)
 
+    def test_email_attribute(self):
+        """Tests the email attribute of User class."""
+        self.user.email = "example@example.com"
+        self.assertEqual(self.user.email, "example@example.com")
+        self.assertRaises(AttributeError, setattr, self.user, "email", 123)
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_password_attribute(self):
+        """Tests the password attribute of User class."""
+        self.user.password = "secret_password"
+        self.assertEqual(self.user.password, "secret_password")
+        self.assertRaises(AttributeError, setattr, self.user, "password", 123)
+
+    def test_first_name_attribute(self):
+        """Tests the first_name attribute of User class."""
+        self.user.first_name = "John"
+        self.assertEqual(self.user.first_name, "John")
+        self.assertRaises(AttributeError, setattr, self.user, "first_name", 123)
+
+    def test_last_name_attribute(self):
+        """Tests the last_name attribute of User class."""
+        self.user.last_name = "Doe"
+        self.assertEqual(self.user.last_name, "Doe")
+        self.assertRaises(AttributeError, setattr, self.user, "last_name", 123)
